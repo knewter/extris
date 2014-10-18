f0ee73454232d28606f4df8aa35522c77cd1aec4
Josh Adams <josh.rubyist@gmail.com>
2014-10-18 14:08:27 -0500

    Render each shape with appropriate color

--- lib/extris/shapes.ex
+++ lib/extris/shapes.ex
@@ -31,6 +31,11 @@ defmodule Extris.Shapes do
     [1, 1]
   ]
 
+  @tee [
+    [1, 1, 1],
+    [0, 1, 0]
+  ]
+
   @shapes %{
     ell: [@ell, rotate(@ell, 90), rotate(@ell, 180), rotate(@ell, 270)],
     jay: [@jay, rotate(@jay, 90), rotate(@jay, 180), rotate(@jay, 270)],
@@ -38,6 +43,7 @@ defmodule Extris.Shapes do
     zee: [@zee, rotate(@zee, 90), rotate(@zee, 180), rotate(@zee, 270)],
     bar: [@bar, rotate(@bar, 90), rotate(@bar, 180), rotate(@bar, 270)],
     oh:  [@oh,  rotate(@oh, 90),  rotate(@oh, 180),  rotate(@oh, 270)],
+    tee: [@tee,  rotate(@tee, 90),  rotate(@tee, 180),  rotate(@tee, 270)],
   }
 
   def shapes, do: @shapes
--- lib/extris/window.ex
+++ lib/extris/window.ex
@@ -7,7 +7,7 @@ defmodule Extris.Window do
   """
 
   @title 'ExTris'
-  @side 50.0
+  @side 25.0
 
   require Record
   Record.defrecordp :wx, Record.extract(:wx, from_lib: "wx/include/wx.hrl")
@@ -75,16 +75,17 @@ defmodule Extris.Window do
 
   def draw_ell(dc) do
     pen = :wx_const.wx_black_pen
-    brush = :wxBrush.new({30, 175, 23, 127})
     canvas = :wxGraphicsContext.create(dc)
     font = :wx_const.wx_italic_font
     :wxGraphicsContext.setPen(canvas, pen)
-    :wxGraphicsContext.setBrush(canvas, brush)
     :wxGraphicsContext.setFont(canvas, font, {0, 0, 50})
-    draw_shape(canvas, Shapes.shapes.ell, 3, 3)
+    Enum.each(Enum.with_index(Shapes.shapes), fn({{name, shape}, i}) ->
+      draw_colored_shape(canvas, name, shape, 1 + 3*i, 1 + 3*i)
+    end)
   end
 
   def draw_shape(canvas, shape, x, y) do
+    IO.inspect shape
     # Specify position in 'grid units'
     for {row, row_i} <- Enum.with_index(shape |> hd) do
       for {col, col_i} <- Enum.with_index(row) do
@@ -96,10 +97,23 @@ defmodule Extris.Window do
       end
     end
   end
+  def draw_colored_shape(canvas, brush_name, shape, x, y) do
+    brush = brush_for(brush_name)
+    :wxGraphicsContext.setBrush(canvas, brush)
+    draw_shape(canvas, shape, x, y)
+  end
 
   def draw_square(canvas, x, y) do
     true_x = @side * x
     true_y = @side * y
     :wxGraphicsContext.drawRectangle(canvas, true_x, true_y, @side, @side)
   end
+
+  def brush_for(:bar), do: :wxBrush.new({0, 240, 255, 255})
+  def brush_for(:jay), do: :wxBrush.new({12, 0, 255, 255})
+  def brush_for(:ell), do: :wxBrush.new({255, 150, 0, 255})
+  def brush_for(:ess), do: :wxBrush.new({5, 231, 5, 255})
+  def brush_for(:zee), do: :wxBrush.new({255, 17, 17, 255})
+  def brush_for(:oh),  do: :wxBrush.new({247, 255, 17, 255})
+  def brush_for(:tee), do: :wxBrush.new({100, 255, 17, 255})
 end
